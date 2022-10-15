a112a2011a94074f6e3a2a02b5143cba85982d57
mmm <margaret.m.matson@gmail.com>
2022-10-15 14:39:33 -0500

    added confirms and loop for user to select character types to include in password

--- script.js
+++ script.js
@@ -15,12 +15,27 @@ function writePassword() {
     while (length <8 || length >128) {
     var length = prompt("Your password must be 8-128 characters. Please enter a different number.");
     }
+    
+    //If password length entered between 8 and 128, confirm password criteria
+    if (length >=8 && length <=128) {
+    var incSymbols = confirm("Do you want symbols in your password?");
+    var incNumbers = confirm("Do you want numbers in your password?");
+    var lowercase = confirm("Do you want lowercase letters in your password?");
+    var uppercase = confirm("Do you want uppercase letters in your password?");
+    }
+
+    //Loop to confirm password criteria if no character types selected
+    while (!incSymbols && !incNumbers && !lowercase && !uppercase) {
+    var incSymbols = confirm("You must confirm at least one character type. Do you want symbols in your password?");
+    var incNumbers = confirm("You must confirm at least one character type. Do you want numbers in your password?");
+    var lowercase = confirm("You must confirm at least one character type. Do you want lowercase letters in your password?");
+    var uppercase = confirm("You must confirm at least one character type. Do you want uppercase letters in your password?");
+    }
 
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
